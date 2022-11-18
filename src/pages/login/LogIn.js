66f9fd20dfa80e5266e938e94024b55d01e994b7
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-18 18:42:56 +0600

    login

--- src/pages/login/LogIn.js
+++ src/pages/login/LogIn.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 const LogIn = () => {
     return (
-        <div>
+        <div className='d-lg-flex  '>
              <img src="Mobile login-bro.png" alt="" className='w-50 h-50 m-5 ps-5'/>
+
+             <div className=' m-5 pt-5'>
+             <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+             </div>
         
         </div>
     );
