4e2361efc8d79d661eec2b1ea43a92d4e6509618
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-18 23:09:49 +0600

    navbarupdate

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import {Button, Col,Row} from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer';
 import Header from '../pages/Shared/Header/Header';
 
 const Main = () => {
     return (
-       <div>
-        <Container>
-            <Header></Header>
-            <Outlet></Outlet>
+       <div >
+            <Header ></Header>
+           <Outlet></Outlet>
             <Footer></Footer>
-            </Container>
+            
        </div>
     );
 };
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -8,24 +8,28 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-           
-           <Navbar bg="light" expand="lg">
-      <Container>
-        <Link to='/' className='fw-bold text-decoration-none text-dark fs-3'>SETSKILL</Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <Link to='/' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>Home</Link>
+                  <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
+            <Container>
+            <Link to='/' className='fw-bold text-decoration-none text-dark fs-3'>SETSKILL</Link>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    <Link to='/' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>Home</Link>
           <Link to='/blogs' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>Blogs</Link>
 
           <Link to='/courses' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>Courses</Link>
           <Link to='/FAQ' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>FAQ</Link>
-          <Link to='/login' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>Login</Link>
-          <Link to='/signup' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>Sign In</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+
+            </Nav>
+               <Nav>
+               <Link to='/login' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>Login</Link>
+               <Link to='/signup' className='ms-3 text-decoration-none fs-6 text-dark fw-semibold'>SignUp</Link>
+               </Nav>
+               <div className='d-lg-none'>
+               </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
 
 
         </div>
