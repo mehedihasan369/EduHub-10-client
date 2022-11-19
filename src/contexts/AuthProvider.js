368f6b26144acaa97db179979fde6fa921082bbe
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-20 00:07:59 +0600

    signup

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged, signInWithPopup, signOut} from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithPopup, signOut} from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -10,6 +10,10 @@ const AuthProvider = ({ children }) => {
 
     const providerLogin = (provider) => {
         return signInWithPopup(auth, provider);
+    }
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const logOut = () => {
@@ -30,7 +34,7 @@ const AuthProvider = ({ children }) => {
 
     }, [])
 
-    const authInfo = { user, providerLogin,logOut };
+    const authInfo = { user, providerLogin,logOut,createUser };
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,49 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 import { FaGoogle } from "react-icons/fa";
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const SignUp = () => {
+  const {createUser} = useContext(AuthContext);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photoURL = form.photoURL.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, photoURL, email, password);
+
+    createUser(email, password)
+    .then( result => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+    })
+    .catch( e => console.error(e));
+}
     return (
         <div className='d-lg-flex  '>
            <img src="./Mobile login-pana.png" alt="" className='w-50 h-50 m-5 ps-5'/>
             <div className=' m-5 pt-5'>
                 <h4>Already registered ?? Then go to <Link to='/login'>login.</Link></h4>
-            <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Photo URL</Form.Label>
-        <Form.Control type="" placeholder="Enter Photo URL" />
+                <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Your Name</Form.Label>
+                <Form.Control name="name" type="text" placeholder="Your Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Photo URL</Form.Label>
+                <Form.Control name="photoURL" type="text" placeholder="Phot URL" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control name="email" type="email" placeholder="Enter email" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="password" type="password" placeholder="Password" required />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Register
+            </Button>
+            <Form.Text className="text-danger">
+
+            </Form.Text>
+        </Form>
+
+
+
  
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+     
 
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control type="password" placeholder="Confirm Password" />
-      </Form.Group>
-
-
-
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
     <h5>or <Button  className='m-2' variant="outline-primary"> <FaGoogle></FaGoogle> Login with Google</Button></h5>
 
             </div>
